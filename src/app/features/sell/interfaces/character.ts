a65384479c5713d40ac4b67aef9eeef8396fcd71
Gutthred <fabioappelgren@gmail.com>
2023-04-04 12:26:05 -0300

    Week 4 commit

---
+++ src/app/features/sell/interfaces/character.ts
@@ -0,0 +1,12 @@
+export interface Character {
+    id?: number,
+    name: string,
+    clan: string,
+    affiliation: string[],
+    nature: string[],
+    patent: string,
+    mission: number,
+    kill: number,
+    img: string
+
+}
